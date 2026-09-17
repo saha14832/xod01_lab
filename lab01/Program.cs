bc084df3d79d7a40568a42944faf83da51db7405
Grimchic <vladis.nr@mail.ru>
2026-09-17 12:50:19 +0300

    Lab01:first program with Environment

--- lab01/Program.cs
+++ lab01/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine
+﻿Console.WriteLine($"компьютер: {Environment.MachineName}");
+
+Console.WriteLine($"пользователь: {Environment.UserName}");
+
+Console.WriteLine($"Дата и время: {DateTime.Now:dd.MM.yyyy HH:mm}");
