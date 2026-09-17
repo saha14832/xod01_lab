4d2e3e97987b1901417eea183536a8193f9da7ed
Grimchic <vladis.nr@mail.ru>
2026-09-17 13:05:02 +0300

    Lab01:systeminfo investigations

--- lab01/Program.cs
+++ lab01/Program.cs
@@ -3,3 +3,14 @@
 Console.WriteLine($"пользователь: {Environment.UserName}");
 
 Console.WriteLine($"Дата и время: {DateTime.Now:dd.MM.yyyy HH:mm}");
+
+
+Console.WriteLine($"OC: {Environment.OSVersion}");
+Console.WriteLine($"64-битная OC: {Environment.Is64BitOperatingSystem}");
+
+Console.WriteLine($"логических процесов: {Environment.ProcessorCount}");
+
+Console.WriteLine($"PID процесса : {Environment.ProcessId}");
+long memory = Environment.WorkingSet / 1024 / 1024;
+Console.WriteLine($"Память процесса: {memory} МБ");
+Console.WriteLine();
